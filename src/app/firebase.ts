16773a0f6edc1582cf90272fbf6684fbfb242428
kanako-hg <kanako-hg@LAPTOP-9CIVBTQP>
2024-03-19 19:57:07 +0900

    FirebaseのAPIKey修正5

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -10,6 +10,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+console.log(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
