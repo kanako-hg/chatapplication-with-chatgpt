2c72a6ad5acdef924af2683c4cebf881fcfe2108
kanako-hg <kanako-hg@LAPTOP-9CIVBTQP>
2024-03-19 20:13:39 +0900

    FirebaseのAPIKey修正6

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -3,16 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: String(process.env.NEXT_PUBLIC_FIREBASE_API_KEY),
-  authDomain: String(process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN),
-  projectId: String(process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID),
-  storageBucket: String(process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET),
-  messagingSenderId: String(
-    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
-  ),
-  appId: String(process.env.NEXT_PUBLIC_FIREBASE_APP_ID),
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-console.log(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
